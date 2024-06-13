c1b3a6e33be57478f7fffc6660db216af7ba0778
DeyseDAS <deysedsaamor22@hotmail.com>
2024-06-12 22:56:56 -0300

    aula inicial python

---
+++ SemanaPython/atividades/hello.py
@@ -0,0 +1,31 @@
+#print("Ola mundo ?")
+
+#texto = input("digite um valor: ")
+
+#print(texto)
+#print(type(texto))
+
+nome = "deysechloebruno"
+idade = 33
+
+#print(type(nome))
+#print(type(idade))
+
+ex = 50
+#print(type(str(ex)))
+
+#print(10*20)
+#print(20-340)
+
+#valor1 = int(input("informe o primeiro valor: "))
+#valor2 =int(input("informe o segundo valor: "))
+
+#print(valor1*valor2)
+
+peso = int(input("informr peso: "))
+altura = int(input("informar altura: "))
+imc = (peso/altura*altura)
+
+print(imc)
+
+
